61664ad31c74c30f389813b0fd1c87992a95be89
chamsjadla <chamsjadla@gmail.com>
2022-11-01 22:16:27 +0100

    first commit

---
+++ src/components/FullName.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const FullName = () => {
+  return <div>jadla chams</div>;
+};
+
+export default FullName;
---
+++ src/components/ProfilePhoto.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ProfilePhoto() {
+  return (
+    <div>
+      <img src="./chams.png" className='chams' alt="myprofile" />
+    </div>
+  )
+}
+
+export default ProfilePhoto
